92f9fca660fec04270c1fba447fa1fa255ee27bf
TAMIRAT-ALEMAYEHU <tamirat002941@gmail.com>
2021-08-22 23:39:41 +0300

    Two c code added to the new testing branch

---
+++ 0x00-testing/my-c-code1.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+
+int main()
+{
+int x = 90;
+
+int y = x/4;
+printf("MY numbers are :%!", y);
+return 0;
+}
---
+++ 0x01-testing/my-c-code2.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+
+int main()
+{
+int x = 360;
+
+int y = x/4;
+printf("Correct Degree :%!", y);
+return 0;
+}
